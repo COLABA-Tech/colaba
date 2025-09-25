f93ddfb4febc3b7526b2ba6c4b637f4f717b89c9
Dmitriy Kolmakov <kolmakov125@gmail.com>
2025-09-25 18:26:40 +0300

    HPS-7 feat: set up project with Spring Boot

---
+++ src/main/java/com/example/colaba/ColabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.colaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColabaApplication.class, args);
+	}
+
+}
