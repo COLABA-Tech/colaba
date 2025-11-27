a218d51a7e4dd9ce50e4b0c791c74561483b71cb
Dmitriy Kolmakov <kolmakov125@gmail.com>
2025-11-27 04:08:56 +0300

    fix: fix package scan

--- project-service/src/main/java/com/example/colaba/project/ProjectServiceApplication.java
+++ project-service/src/main/java/com/example/colaba/project/ProjectServiceApplication.java
@@ -2,12 +2,15 @@ package com.example.colaba.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients  // TODO
+@EntityScan(basePackages = {"com.example.colaba.shared.entity", "com.example.colaba.project.entity"})
+@EnableJpaRepositories(basePackages = "com.example.colaba.project.repository")
 public class ProjectServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectServiceApplication.class, args);
--- task-service/src/main/java/com/example/colaba/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/example/colaba/task/TaskServiceApplication.java
@@ -2,12 +2,15 @@ package com.example.colaba.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients  // TODO
+@EntityScan(basePackages = {"com.example.colaba.shared.entity", "com.example.colaba.task.entity"})
+@EnableJpaRepositories(basePackages = "com.example.colaba.task.repository")
 public class TaskServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskServiceApplication.class, args);
