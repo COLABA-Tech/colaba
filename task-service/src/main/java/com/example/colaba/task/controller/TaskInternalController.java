fa7035930e129f0613a769213137fe1a0d96c224
Dmitriy Kolmakov <kolmakov125@gmail.com>
2025-12-11 22:59:22 +0300

    HPS-56 feat: set up config server

--- task-service/src/main/java/com/example/colaba/task/controller/TaskInternalController.java
+++ task-service/src/main/java/com/example/colaba/task/controller/TaskInternalController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/internal/tasks")
+@RequestMapping("/api/tasks/internal")
 @RequiredArgsConstructor
 public class TaskInternalController {
 
