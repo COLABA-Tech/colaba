0ef213bfdc73b8d44c862727e83cb91b7b8b9a0c
bozhenka <bozhena.andreeva@gmail.com>
2025-12-12 04:01:33 +0300

    HPS-54 feat: fix mapper

--- shared/src/main/java/com/example/colaba/shared/mapper/TaskMapper.java
+++ shared/src/main/java/com/example/colaba/shared/mapper/TaskMapper.java
@@ -3,9 +3,7 @@ package com.example.colaba.shared.mapper;
 import com.example.colaba.shared.dto.task.TaskResponse;
 import com.example.colaba.shared.entity.task.Task;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.springframework.data.domain.Page;
-
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
 
